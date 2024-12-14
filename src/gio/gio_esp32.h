e2f0c761ebfb5de5707ba4a896b709157d85d013
AlexGyver <alexgyvershow@ya.ru>
2024-12-14 14:13:25 +0300

    upd

--- src/gio/gio_esp32.h
+++ src/gio/gio_esp32.h
@@ -11,15 +11,15 @@
 
 namespace gio {
 
-void __esp32_rtc_idf3(uint8_t pin) {
 #if ESP_IDF_VERSION_MAJOR < 4
+void __attribute__((weak)) __esp32_rtc_idf3(uint8_t pin) {
     uint32_t rtc_reg(rtc_gpio_desc[pin].reg);
     if (rtc_reg) {
         ESP_REG(rtc_reg) = ESP_REG(rtc_reg) & ~(rtc_gpio_desc[pin].mux);
         ESP_REG(rtc_reg) = ESP_REG(rtc_reg) & ~(rtc_gpio_desc[pin].pullup | rtc_gpio_desc[pin].pulldown);
     }
-#endif
 }
+#endif
 
 // mode
 _GIO_INLINE void mode(uint8_t pin, uint8_t mode) {
@@ -30,7 +30,9 @@ _GIO_INLINE void mode(uint8_t pin, uint8_t mode) {
 #if defined(CONFIG_IDF_TARGET_ESP32C3) || defined(CONFIG_IDF_TARGET_ESP32C6)
             GPIO.enable_w1tc.val = (1ul << pin);
 #else
+#if ESP_IDF_VERSION_MAJOR < 4
             __esp32_rtc_idf3(pin);
+#endif
             if (pin < 32) GPIO.enable_w1tc = (1ul << pin);
             else GPIO.enable1_w1tc.val = (1ul << (pin - 32));
 #endif
@@ -41,7 +43,9 @@ _GIO_INLINE void mode(uint8_t pin, uint8_t mode) {
             GPIO.enable_w1ts.val = (1ul << (pin));
 #else
             if (pin <= 33) {
+#if ESP_IDF_VERSION_MAJOR < 4
                 __esp32_rtc_idf3(pin);
+#endif
                 if (pin < 32) GPIO.enable_w1ts = (1ul << pin);
                 else GPIO.enable1_w1ts.val = (1ul << (pin - 32));
             }
