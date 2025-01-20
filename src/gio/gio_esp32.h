9b70e5ba27a55fe63ee9dee9b345e51751ad1a9f
AlexGyver <alexgyvershow@ya.ru>
2025-01-20 23:39:52 +0300

    upd

--- src/gio/gio_esp32.h
+++ src/gio/gio_esp32.h
@@ -9,43 +9,40 @@
 
 #include "gio_defs.h"
 
-namespace gio {
-
 #if ESP_IDF_VERSION_MAJOR < 4
-void __attribute__((weak)) __esp32_rtc_idf3(uint8_t pin) {
-    uint32_t rtc_reg(rtc_gpio_desc[pin].reg);
-    if (rtc_reg) {
-        ESP_REG(rtc_reg) = ESP_REG(rtc_reg) & ~(rtc_gpio_desc[pin].mux);
-        ESP_REG(rtc_reg) = ESP_REG(rtc_reg) & ~(rtc_gpio_desc[pin].pullup | rtc_gpio_desc[pin].pulldown);
+#define _ESP32_IDF_V3_REG()                                                                               \
+    uint32_t rtc_reg(rtc_gpio_desc[pin].reg);                                                             \
+    if (rtc_reg) {                                                                                        \
+        ESP_REG(rtc_reg) = ESP_REG(rtc_reg) & ~(rtc_gpio_desc[pin].mux);                                  \
+        ESP_REG(rtc_reg) = ESP_REG(rtc_reg) & ~(rtc_gpio_desc[pin].pullup | rtc_gpio_desc[pin].pulldown); \
     }
-}
+#else
+#define _ESP32_IDF_V3_REG()
 #endif
+
+namespace gio {
 
 // mode
 _GIO_INLINE void mode(uint8_t pin, uint8_t mode) {
-    if (!digitalPinIsValid(pin)) return;
-
     switch (mode) {
         case INPUT:
-#if defined(CONFIG_IDF_TARGET_ESP32C3) || defined(CONFIG_IDF_TARGET_ESP32C6)
-            GPIO.enable_w1tc.val = (1ul << pin);
+#if CONFIG_IDF_TARGET_ESP32C3
+            GPIO.enable_w1tc.val = (1ul << (pin));
 #else
-#if ESP_IDF_VERSION_MAJOR < 4
-            __esp32_rtc_idf3(pin);
-#endif
-            if (pin < 32) GPIO.enable_w1tc = (1ul << pin);
-            else GPIO.enable1_w1tc.val = (1ul << (pin - 32));
+            if (digitalPinIsValid(pin)) {
+                _ESP32_IDF_V3_REG();
+                if (pin < 32) GPIO.enable_w1tc = (1ul << pin);
+                else GPIO.enable1_w1tc.val = (1ul << (pin - 32));
+            }
 #endif
             break;
 
         case OUTPUT:
-#if defined(CONFIG_IDF_TARGET_ESP32C3) || defined(CONFIG_IDF_TARGET_ESP32C6)
+#if CONFIG_IDF_TARGET_ESP32C3
             GPIO.enable_w1ts.val = (1ul << (pin));
 #else
-            if (pin <= 33) {
-#if ESP_IDF_VERSION_MAJOR < 4
-                __esp32_rtc_idf3(pin);
-#endif
+            if (digitalPinIsValid(pin)) {
+                _ESP32_IDF_V3_REG();
                 if (pin < 32) GPIO.enable_w1ts = (1ul << pin);
                 else GPIO.enable1_w1ts.val = (1ul << (pin - 32));
             }
@@ -60,34 +57,32 @@ _GIO_INLINE void mode(uint8_t pin, uint8_t mode) {
 
 // read
 _GIO_INLINE int read(uint8_t pin) {
-    if (!digitalPinIsValid(pin)) return 0;
-#if defined(CONFIG_IDF_TARGET_ESP32C3) || defined(CONFIG_IDF_TARGET_ESP32C6)
+#if CONFIG_IDF_TARGET_ESP32C3
     return (GPIO.in.val >> pin) & 0x1;
 #else
     if (pin < 32) return (GPIO.in >> pin) & 0x1;
-    else return (GPIO.in1.val >> (pin - 32)) & 0x1;
+    else if (pin < 46) return (GPIO.in1.val >> (pin - 32)) & 0x1;
 #endif
+    return 0;
 }
 
 // low
 _GIO_INLINE void low(uint8_t pin) {
-    if (!digitalPinIsValid(pin)) return;
-#if defined(CONFIG_IDF_TARGET_ESP32C3) || defined(CONFIG_IDF_TARGET_ESP32C6)
+#if CONFIG_IDF_TARGET_ESP32C3
     GPIO.out_w1tc.val = (1ul << pin);
 #else
     if (pin < 32) GPIO.out_w1tc = (1ul << pin);
-    else GPIO.out1_w1tc.val = (1ul << (pin - 32));
+    else if (pin < 46) GPIO.out1_w1tc.val = (1ul << (pin - 32));
 #endif
 }
 
 // high
 _GIO_INLINE void high(uint8_t pin) {
-    if (!digitalPinIsValid(pin)) return;
-#if defined(CONFIG_IDF_TARGET_ESP32C3) || defined(CONFIG_IDF_TARGET_ESP32C6)
+#if CONFIG_IDF_TARGET_ESP32C3
     GPIO.out_w1ts.val = (1ul << pin);
 #else
     if (pin < 32) GPIO.out_w1ts = (1ul << pin);
-    else GPIO.out1_w1ts.val = (1ul << (pin - 32));
+    else if (pin < 46) GPIO.out1_w1ts.val = (1ul << (pin - 32));
 #endif
 }
 
